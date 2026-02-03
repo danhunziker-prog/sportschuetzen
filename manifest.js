08f6e1e25af9bdcc0a2f430be2afab6d2806b49b
danhunziker-prog <dan.hunziker@bluewin.ch>
2026-02-03 21:11:02 +0100

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,8 @@
+{
+  "name": "OneSignal Test",
+  "short_name": "OS Test",
+  "start_url": "/sportschuetzen/index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#ffffff"
+}
